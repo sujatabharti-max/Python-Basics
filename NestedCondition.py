e5d94f34606aad9367cf815ad34e898cb95d4f74
sujatabharti-max <sujatabharti@gmail.com>
2026-04-24 20:20:35 +0530

    Add files via upload

---
+++ NestedCondition.py
@@ -0,0 +1,8 @@
+number= -4
+if (number <0):
+    print ("number is negative")
+elif (number==0):
+    print ("number is zero")
+else:
+   print ("number is positive") 
+print ("this is always printed")
